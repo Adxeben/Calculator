5ab0fa36768e94dbf591a06253b26fef35cf1bd6
Omotola <omotolaadebulu@gmail.com>
2022-09-18 06:22:16 +0100

    finished calc added styles and scripts

---
+++ src/js/script.js
@@ -0,0 +1,30 @@
+const display = document.getElementById('calc__display');
+
+const buttons = Array.from(document.getElementsByClassName('calc__btn'));
+
+buttons.map(button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = " ";
+                break;
+
+            case '←':
+                display.innerText = display.innerText.slice(0, -1);
+                break;
+
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+
+                }catch{
+                    display.innerText = 'Error';
+                }
+                break;
+
+            default:
+                display.innerText += e.target.innerText;
+        }
+        
+    })
+})
